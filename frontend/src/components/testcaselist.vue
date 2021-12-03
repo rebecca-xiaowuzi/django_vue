867caf6cba98aafa9c6bc03ff823951889319a5f
814981298@qq.com <814981298@qq.com>
2021-12-03 17:05:52 +0800

    不知道在干啥

--- frontend/src/components/testcaselist.vue
+++ frontend/src/components/testcaselist.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-form><el-form-item><el-button type="primary" @click="add2">新增</el-button></el-form-item><el-form-item><el-button type="primary" @click="add2">新增sql</el-button></el-form-item></el-form>
+  <el-form><el-form-item><el-button type="primary" @click="add">新增</el-button></el-form-item><el-form-item><el-button type="primary" @click="add2">新增sql</el-button></el-form-item></el-form>
 
 </template>
 
